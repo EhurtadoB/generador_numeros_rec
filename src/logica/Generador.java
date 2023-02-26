fa5a49370a36b974ee4f288387aab2468050deb1
EhurtadoB <eleanithahurtado@gmail.com>
2023-02-26 13:18:31 -0500

    interfaz

--- src/logica/Generador.java
+++ src/logica/Generador.java
@@ -5,10 +5,30 @@
  */
 package logica;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Eleana Hurtado
  */
 public class Generador {
-    
+   
+    public ArrayList<Integer> genCongruencialMixto(int x, int a, int c, int m, int n){
+        
+        ArrayList<Integer> numerosEnteros = new ArrayList<>();
+        ArrayList<Float> numerosRango = new ArrayList<>();
+        ArrayList<Integer>numA= new ArrayList<>();
+        
+        numA.add(a);
+        numerosEnteros.add(x);
+        for(int i=0; i<n; i++){
+            numA.add((numerosEnteros.get(i)*a)+c);
+            numerosEnteros.add(numA.get(i+1)%m);
+        }
+        for (int i = 0; i < numerosEnteros.size(); i++) {
+            System.out.println(numA.get(i)+"|"+numerosEnteros.get(i));
+        }
+        return numerosEnteros;
+       
+    }
 }
--- src/vista/JFrPrincipal.java
+++ src/vista/JFrPrincipal.java
@@ -5,6 +5,9 @@
  */
 package vista;
 
+import javax.swing.JOptionPane;
+import logica.Generador;
+
 /**
  *
  * @author Eleana Hurtado
@@ -14,8 +17,12 @@ public class JFrPrincipal extends javax.swing.JFrame {
     /**
      * Creates new form JFrPrincipal
      */
+    static String metodo;
+    
     public JFrPrincipal() {
         initComponents();
+        this.setLocationRelativeTo(null);
+        
     }
 
     /**
@@ -27,21 +34,103 @@ public class JFrPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabelTitulo = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jButtonCongruMixto = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(153, 255, 255));
+
+        jLabelTitulo.setFont(new java.awt.Font("Yu Gothic", 1, 18)); // NOI18N
+        jLabelTitulo.setText("Generador de números rectangulares");
+
+        jPanel2.setBackground(new java.awt.Color(153, 255, 255));
+
+        jButtonCongruMixto.setText("Congruencial mixto");
+        jButtonCongruMixto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonCongruMixtoActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Congruencial Multiplicativo ");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jButtonCongruMixto, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 183, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addComponent(jButtonCongruMixto)
+                .addGap(18, 18, 18)
+                .addComponent(jButton2)
+                .addGap(0, 131, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(47, 47, 47)
+                        .addComponent(jLabelTitulo))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(116, 116, 116)
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(55, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addComponent(jLabelTitulo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(89, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButtonCongruMixtoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonCongruMixtoActionPerformed
+        // TODO add your handling code here:
+        
+        metodo="mixto";
+        JFrMostrarResultados mostrar = new JFrMostrarResultados();
+        mostrar.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_jButtonCongruMixtoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -68,9 +157,9 @@ public class JFrPrincipal extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(JFrPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-
+        //</editor-fol     
         /* Create and display the form */
+
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new JFrPrincipal().setVisible(true);
@@ -79,5 +168,10 @@ public class JFrPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButtonCongruMixto;
+    private javax.swing.JLabel jLabelTitulo;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
